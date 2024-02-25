aab8e79f04236ba41cbadc7b6717183a59a39094
Ana Gianni <giannianaclara@gmail.com>
2024-02-25 11:57:08 -0300

    primer commit

---
+++ Proyecto2/Doctor2/Doctor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Doctor2
+{
+    public class Doctor
+    {
+        public int edad { get; set; }
+
+        public string especialidad { get; set; }
+
+        public Doctor(int pedad, string pespecialidad)
+        {
+            edad = pedad;
+            especialidad = pespecialidad;
+        }
+
+        public void MostrarDoctor()
+        {
+            Console.WriteLine($"El doctor tiene {edad} años" +
+                $" y su especialidad es {especialidad}");
+        }
+    }
+}
---
+++ Proyecto2/Doctor2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Doctor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Doctor doctor1 = new Doctor(50, "traumatologia");
+            doctor1.MostrarDoctor();
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
